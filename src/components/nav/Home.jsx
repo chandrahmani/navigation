29c50e5c70c2bc6a3c65f4bac71337547f68b5ec
chandrahmani <chandrahmani254@gmail.com>
2022-12-09 15:26:21 +0530

    home page done

--- src/components/nav/Home.jsx
+++ src/components/nav/Home.jsx
@@ -14,8 +14,8 @@ const Home = () => {
         </div>
       </header>
 
-      <section className="">
-        <div>
+      <section className="section-wrapper">
+        <div className="txt">
           <h2>Navigation Builder</h2>
         </div>
         <div>
@@ -23,12 +23,15 @@ const Home = () => {
           <h3>navigation builder</h3>
         </div>
 
-        <div>
+        <div className="btn-container">
           <button>Add Naigation</button>
         </div>
 
-        <div>
-          <img src="" alt="abc" />
+        <div className="box">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPcB9pt3Tasegrjl6637LB1iJqCND1jp2oLA&usqp=CAU"
+            alt="abc"
+          />
         </div>
       </section>
     </div>
