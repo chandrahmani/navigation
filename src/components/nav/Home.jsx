b54c8ccda3d7fb5d1c8c033724e662bd63ed852b
chandrahmani <chandrahmani254@gmail.com>
2022-12-09 11:42:33 +0530

    update home page

---
+++ src/components/nav/Home.jsx
@@ -0,0 +1,37 @@
+const Home = () => {
+  return (
+    <div>
+      <header className="lists">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+
+        <div>
+          <p>c</p>
+        </div>
+      </header>
+
+      <section className="">
+        <div>
+          <h2>Navigation Builder</h2>
+        </div>
+        <div>
+          <p>Create navigation based on your app requirements using</p>
+          <h3>navigation builder</h3>
+        </div>
+
+        <div>
+          <button>Add Naigation</button>
+        </div>
+
+        <div>
+          <img src="" alt="abc" />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
